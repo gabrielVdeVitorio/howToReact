4617296a54b7f867f56552c834ff6beb2b1d09ee
bendominique <benbenjamim936@gmail.com>
2026-09-10 08:58:09 -0300

    Still trying to make a looping function while holding.

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -2,45 +2,64 @@ import styles from './Button.module.css';
 import React, { useRef } from 'react';
 
 interface ButtonProps {
+    whetherRepeat?: string;
     holdTime?: number;
     content?: string;
-    onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
-    onDoubleClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    onClick?: (e: React.PointerEvent<HTMLButtonElement>) => void;
+    onDoubleClick?: (e: React.PointerEvent<HTMLButtonElement>) => void;
     onPressedAndHold?: () => void;
 }
 
 function Button(
     {
-    onPressedAndHold,
-    onDoubleClick,
-    onClick,
-    content = "Click here",
-    holdTime = 1000,
+        onPressedAndHold,
+        onDoubleClick,
+        onClick,
+        content = "Click here",
+        holdTime = 1000,
+        whetherRepeat = 'repeat'
     }: ButtonProps)
-    {
+{
     const timerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
     const isHoldRef = useRef<boolean>(false);
-    const isHolding = () => { while (isHoldRef.current) { if (onPressedAndHold) onPressedAndHold} }
+    const holdRepeatTicket = useRef<number>(null);
+    const whetherRepeatFunction =
+    {
+        repeat: () => { holdRepeatTicket.current = requestAnimationFrame(onPressedAndHold) },
+        norepeat: () => { onPressedAndHold() },
+        cancelrepeat: () =>
+        {
+            cancelAnimationFrame(holdRepeatTicket.current);
+            this.cancelnorepeat();
+        },
+        cancelnorepeat: () =>
+        {
+            clearTimeout(timerRef.current);
+            timerRef.current = null;
+        }
+    }
     const startHold = () =>
     {
         isHoldRef.current = false;
-        timerRef.current = setTimeout(() =>
+        if (onPressedAndHold)
         {
-            isHoldRef.current = true;
-            isHolding();
-        }, holdTime);
+            timerRef.current = setTimeout(() =>
+            {
+                isHoldRef.current = true;
+                whetherRepeatFunction[whetherRepeat]();
+            }, holdTime);
+        }
     };
 
     const cancelHold = () =>
     {
         if (timerRef.current)
         {
-            clearTimeout(timerRef.current);
-            timerRef.current = null;
+            whetherRepeatFunction[`cancel${whetherRepeat}`];
         }
     };
 
-    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) =>
+    const handleClick = (e: React.PointerEvent<HTMLButtonElement>) =>
     {
         if (isHoldRef.current)
         {
@@ -52,6 +71,8 @@ function Button(
 
     return (
         <button
+            onPointerDown={cancelHold}
+            onPointerUp={cancelHold}
             onMouseDown={startHold}
             onMouseUp={cancelHold}
             onMouseLeave={cancelHold}
